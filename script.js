5722f032bd729616a2979805e0c9e0c2f11f1801
Suelen <suelenonyrius@gmail.com>
2021-11-29 21:41:53 -0300

    adiciona requisito 11

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const criarTarefaButton = document.getElementById('criar-tarefa');
 criarTarefaButton.addEventListener('click', criadoraDeTarefa)
+let lista = document.getElementsByTagName('ol')[0];
+
 
 function criadoraDeTarefa () {
     let maisUmaTarefa = document.createElement('li');
-    let listaDetarefas = document.getElementsByTagName('ol')[0]
     let inputTarefaText = document.getElementsByTagName('input')[0];
-
-    listaDetarefas.appendChild(maisUmaTarefa)
+    lista.appendChild(maisUmaTarefa)
     maisUmaTarefa.innerText = inputTarefaText.value;
+    //acessoDinamico - cria acesso dinâmico às li's para manipulação pelo DOM.
     maisUmaTarefa.classList = 'acessoDinamico'
     inputTarefaText.value = ""
 
@@ -29,24 +30,41 @@ function colorGrey (evento) {
 }
 
 }
-
+//--------------tarefas completas -------
 function completed(evento) {
-    const tarefaFeita =  document.querySelector('.acessoDinamico');
-    evento.target.classList.toggle('completed')
+    console.log(evento.target.classList.toggle('completed'))
+
 }
+
+//---------apaga tudo -------------------
 
 let buttonEraseAll = document.querySelector('#apaga-tudo');
 buttonEraseAll.addEventListener('click', eraseAll);
-function eraseAll (evento){
-    let deletLi = document.querySelectorAll('.acessoDinamico');
-    for (let li of deletLi) {
-        if (evento){
-            li.outerHTML = ' '
-
-        }
-    }
+function eraseAll() {
+    let listaLi  = document.querySelectorAll('.acessoDinamico')
     
+    for (let index = 0; index < listaLi.length ; index +=1) {
+        listaLi[index].remove()
+    }
+}
+//-------------- Adicione botão 'remover-finalizados---
+const buttonErasefinal = document.querySelector('#remover-finalizados');
+buttonErasefinal.addEventListener('click', erasePart)
+function erasePart (){
+    let listaLi  = document.querySelectorAll('.completed')
+    
+    for (let index = 0; index < listaLi.length ; index +=1) {
+        listaLi[index].remove()
+    }
 }
 
 
+    
+
+
+
+
+    
+    
+    
 
